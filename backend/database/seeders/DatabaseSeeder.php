8e2632570d29ce4e2dd2cffa99af02da5e8946ae
array <rivorra@marhotels.com>
2023-04-10 20:22:49 +0200

    add instruments and instrument_user custom seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             UserSeeder::class,
+            InstrumentSeeder::class,
         ]);
     }
 }
--- backend/database/seeders/InstrumentSeeder.php
+++ backend/database/seeders/InstrumentSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Instrument;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class InstrumentSeeder extends Seeder
 {
@@ -12,6 +14,23 @@ class InstrumentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Instrument::factory()->create([
+            'id' => 1,
+            'name' => 'voz',
+        ]);
+        Instrument::factory()->create([
+            'id' => 2,
+            'name' => 'guitarra eléctrica',
+        ]);
+        Instrument::factory()->create([
+            'id' => 3,
+            'name' => 'bajo',
+        ]);
+        Instrument::factory()->create([
+            'id' => 4,
+            'name' => 'batería',
+        ]);
+        DB::unprepared(file_get_contents('database/seeders/inserts/insert_instrument_user.sql'));
+
     }
 }
