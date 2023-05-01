678bf95b6832354cf1fbf33cc8d4aa1e53abe64f
array <rivorra@marhotels.com>
2023-05-01 10:22:35 +0200

    add: TimeframeController methods, Postman tested

--- backend/app/Http/Controllers/Api/TimeframeController.php
+++ backend/app/Http/Controllers/Api/TimeframeController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTimeframeRequest;
+use App\Http\Requests\UpdateTimeframeRequest;
 use App\Http\Resources\TimeframeCollection;
+use App\Http\Resources\TimeframeResource;
 use App\Models\Timeframe;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class TimeframeController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): TimeframeCollection
     {
         return TimeframeCollection::make(Timeframe::all());
     }
@@ -20,32 +24,42 @@ class TimeframeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTimeframeRequest $request): TimeframeResource
     {
-        //
+        $validated = $request->validated();
+        $timeframe = Timeframe::create($validated);
+
+        return TimeframeResource::make($timeframe);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): TimeframeResource
     {
-        //
+        return new TimeframeResource(Timeframe::findOrFail($id));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateTimeframeRequest $request, string $id): JsonResponse
     {
-        //
+        $validated = $request->validated();
+        Timeframe::findOrFail($id)->update($validated);
+
+        return response()->json([
+            'data' => new TimeframeResource(Timeframe::findOrFail($id))
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): Response
     {
-        //
+        Timeframe::findOrFail($id)->delete();
+
+        return response()->noContent();
     }
 }
--- backend/app/Http/Requests/StoreTimeframeRequest.php
+++ backend/app/Http/Requests/StoreTimeframeRequest.php
@@ -11,7 +11,7 @@ class StoreTimeframeRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
