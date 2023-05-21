33f38d2cb8da7bdd51d69f59b9a3de0bc3564da9
josep <jcastellcolom@cifpfbmoll.eu>
2023-05-21 12:48:35 +0200

    fix post image & comments view styles

--- frontend/src/app/dashboard/components/comments-view/comments-view.component.ts
+++ frontend/src/app/dashboard/components/comments-view/comments-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ResponseComment } from 'src/app/models';
 
 @Component({
@@ -26,19 +26,23 @@ import { ResponseComment } from 'src/app/models';
     </div>
   `,
 })
-export class PostCommentsViewComponent {
+export class PostCommentsViewComponent implements OnInit {
   @Input() comments: ResponseComment[];
 
   @Output() submitted = new EventEmitter<string>();
 
   message: string;
   showComments = false;
-  showCommentsMessage = 'Show comments...';
+  showCommentsMessage: string;
+
+  ngOnInit(): void {
+    this.showCommentsMessage = `Show ${this.comments.length} comments`;
+  }
 
   toggleShowComments(): void {
     this.showComments = !this.showComments;
     this.showCommentsMessage = !this.showComments
-      ? 'Show comments...'
+      ? `Show ${this.comments.length} comments`
       : 'Hide comments';
   }
 
